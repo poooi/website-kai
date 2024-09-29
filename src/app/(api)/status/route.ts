d61ebf7f8c2208622382298e19bed9263ffcd7c8
かがみ <kagami@kagami.me>
2024-09-29 12:24:26 +0800

    feat: test ip country availability

--- src/app/(api)/status/route.ts
+++ src/app/(api)/status/route.ts
@@ -1,3 +1,6 @@
-export const GET = async () => {
-  return Response.json({ message: 'poi poi poi!' })
+export const runtime = 'edge'
+
+export const GET = async (request: Request) => {
+  const ipCountry = request.headers.get('CF-IPCountry')
+  return Response.json({ message: 'poi poi poi!', country: ipCountry })
 }
