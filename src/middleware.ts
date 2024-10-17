0112cd130326275d1192d9765aa3eeef2b918af6
かがみ <kagami@kagami.me>
2024-10-18 01:28:13 +0800

    feat: setting header at middleware level

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,41 @@
-import { type NextRequest } from 'next/server'
+import { NextResponse, type NextRequest } from 'next/server'
 import { i18nRouter } from 'next-i18n-router'
 
 import { i18nConfig } from './i18n-config'
 
-export function middleware(request: NextRequest) {
-  return i18nRouter(request, i18nConfig)
-}
+const matcher = new RegExp('/((?!api|static|.*\\..*|_next).*)')
 
-// applies this middleware only to files in the app directory
-export const config = {
-  matcher: '/((?!api|static|.*\\..*|_next).*)',
+// FIXME: CF Pages does not always honor next.config.js headers config
+// Check if this is required when miragted to worker
+const poiHeaders = [
+  {
+    key: 'X-Poi-Codename',
+    value: 'Shiratsuyu',
+  },
+  {
+    key: 'X-Poi-Revision',
+    value: process.env.COMMIT_HASH!,
+  },
+  {
+    key: 'X-Poi-Build-Date',
+    value: process.env.BUILD_DATE!,
+  },
+  {
+    key: 'X-Poi-Greetings',
+    value: 'poi?',
+  },
+]
+
+export function middleware(request: NextRequest) {
+  let resp: NextResponse
+  if (matcher.test(request.nextUrl.pathname)) {
+    resp = i18nRouter(request, i18nConfig)
+  } else {
+    resp = NextResponse.next()
+  }
+
+  poiHeaders.forEach((header) => {
+    resp.headers.set(header.key, header.value)
+  })
+  return resp
 }
