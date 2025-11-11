c2c8f901c2f1f6cffaf30a6be338099f28a66692
かがみ <kagami@kagami.me>
2025-11-11 23:00:17 +0800

    fix: field name

--- src/app/(api)/status/route.ts
+++ src/app/(api)/status/route.ts
@@ -2,5 +2,5 @@ export const runtime = 'edge'
 
 export const GET = async (request: Request) => {
   const ipCountry = request.headers.get('CF-IPCountry')
-  return Response.json({ message: 'poi poi poi!', country: ipCountry })
+  return Response.json({ message: 'poi poi poi!', region: ipCountry })
 }
